cd1acb2fcd21d2ed35b30ec3aa681c12688a5a26
kongkongo <55430282+kongkongo@users.noreply.github.com>
2021-02-20 23:27:55 +0800

    Create 31.magicmethods.py

---
+++ 31.magicmethods.py
@@ -0,0 +1,21 @@
+#magicmethods
+
+# In the backend, python is mostly objects and method
+# calls on objects.
+
+# Here we see an example, where the `print()` function
+# is just a call to the magic method `__repr__()`.
+
+class   PrintList(object):
+
+    def     __init__(self,my_list):
+        self.mylist=my_list
+    
+    def     __repr__(self):
+        return  str(self.mylist)
+    
+printlist=PrintList(["a","b","c"])
+print(printlist.__repr__())
+
+结果：
+['a', 'b', 'c']
