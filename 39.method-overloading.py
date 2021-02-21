2917c57a748db28debd8bbe818c485c224c84b78
kongkongo <55430282+kongkongo@users.noreply.github.com>
2021-02-22 00:28:29 +0800

    Create 39.method-overloading.py

---
+++ 39.method-overloading.py
@@ -0,0 +1,46 @@
+class   MyList(list):
+    def   __getitem__(self,index):
+        if  index==0:
+            raise   IndexError
+        if  index>0:
+            index-=1
+        return  list.__getitem__(self,index)
+
+    def   __setitem__(self,index,value):
+        if  index==0:
+            raise   IndexError
+        if  index>0:
+            index-=1
+        list.__setitem__(self,index,value)
+
+x=MyList(['a','b','c'])
+print(x)
+print("-"*10)
+
+x.append('d')
+print(x)
+print("-"*10)
+
+x.__setitem__(4,'e')
+print(x)
+print("-"*10)
+
+print(x[1])
+print(x.__getitem__(1))
+print("-"*10)
+
+print(x[4])
+print(x.__getitem__(4))
+
+结果：
+['a', 'b', 'c']
+----------
+['a', 'b', 'c', 'd']
+----------
+['a', 'b', 'c', 'e']
+----------
+a
+a
+----------
+e
+e
